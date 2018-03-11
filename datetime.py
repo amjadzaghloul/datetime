5f1c26a8bda442e546f3e80885bc315efb9be2e3
Amjad <amjad.zaghloul@gmail.com>
2018-03-11 18:35:58 +0300

    Year_to_day variable added

--- datetime.py
+++ datetime.py
@@ -5,6 +5,6 @@ def daysBetweenDates():
     day = datetime.date.today().day
     date1 = datetime.date(int(raw_input("Enter your year")),int(raw_input("Enter your month")),int(raw_input("Enter your day")))
     date2 = datetime.date(year,month,day)
-    print date2 - date1
-
+    year_to_days = date2 - date1
+    print year_to_days
 daysBetweenDates()
